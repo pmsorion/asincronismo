62f78e4fbd127bb5eb0c0c3285065c5a5ee1eff1
Francisco Garcia <garciafran@hotmail.com>
2020-04-18 18:28:10 -0500

    promise challenge

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,18 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const fetchData = (url_api) => {
-    return new Promise((rosole, reject) => {
-        let xhttp = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
         xhttp.open('GET', url_api, true);
-        xhttp.onreadystatechange = function(event) {
+        xhttp.onreadystatechange = (() => {
             if (xhttp.readyState === 4) {
-                if (xhttp.status === 200) {
-                    callback(null, JSON.parse(xhttp.responseText));
-                } else {
-                    const erro = new Error(`Error ${url_api}`);
-                    return callback(erro, null);
-                }
+                (xhttp.status === 200) 
+                ? resolve(JSON.parse(xhttp.responseText)) 
+                : reject(new Error('Error ', url_api))
             }
-        }
+        });
         xhttp.send();
     });
 }
+
+module.exports = fetchData;
