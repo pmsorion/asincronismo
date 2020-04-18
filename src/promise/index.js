7b6da3528a98b0d43fc422317c4c0721bc3d15d6
Francisco Garcia <garciafran@hotmail.com>
2020-04-18 15:36:43 -0500

    promise2

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
     return new Promise((resolve, reject) => {
-        if (false) {
+        if (true) {
             resolve('hey!!');
         } else {
             reject('whoooop!!');
@@ -8,23 +8,28 @@ const somethingWillHappen = () => {
     });
 };
 
-somethingWillHappen()
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+ somethingWillHappen()
+     .then(response => console.log(response))
+     .catch(err => console.error(err));
 
-const somethingWillHappen2 = () => {
-    return new Promise((resolve, reject) => {
-        if (false) {
-            setTimeout(() => {
-                resolve('true')
-            }, 2000);
-        } else {
-            const error = new  Error('"whooop!!');
-            reject(error);
-        }
-    });
-}
 
-somethingWillHappen2()
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+ const somethingWillHappen2 = () => {
+     return new Promise((resolve, reject) => {
+         if (true) {
+             setTimeout(() => {
+                 resolve('true')
+             }, 2000);
+         } else {
+             const error = new Error('whooop!!');
+             reject(error);
+         }
+     });
+ }
+
+ somethingWillHappen2()
+     .then(response => console.log(response))
+     .catch(err => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+ .then(response => console.log(`Array of results ${response}`))
+ .catch(err => console.log(`Array of error ${err}`))
