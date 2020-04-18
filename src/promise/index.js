d3500eb6f91cf1c68732bbc009c6f5c81ada85f4
Francisco Garcia <garciafran@hotmail.com>
2020-04-17 22:55:50 -0500

    tercer commit

--- src/promise/index.js
+++ src/promise/index.js
@@ -11,3 +11,20 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            setTimeout(() => {
+                resolve('true')
+            }, 2000);
+        } else {
+            const error = new  Error('"whooop!!');
+            reject(error);
+        }
+    });
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
