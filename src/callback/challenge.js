362e41ba3691323631223fb24802ee99afeaa476
Francisco Garcia <garciafran@hotmail.com>
2020-04-17 19:17:04 -0500

    RepoRemoto

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,6 +1,27 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+const API = 'https://rickandmortyapi.com/api/character/';
+
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
-
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function(event) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const erro = new Error(`Error ${url_api}`);
+                return callback(erro, null);
+            }
+        }
+    }
+    xhttp.send();
 }
+
+fetchData(API, function(error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id), function(error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(data2.onreadystatechange)
+    }
+})
